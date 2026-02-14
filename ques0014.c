bb707e293a9506befda50be119624c936149b53b
Arjun Dagar <arjundagar2006@gmail.com>
2026-02-14 13:40:58 +0530

    Add files via upload

---
+++ ques0014.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a[50][50];
+    int i, j, flag = 1;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            if(i == j && a[i][j] != 1)
+                flag = 0;
+            if(i != j && a[i][j] != 0)
+                flag = 0;
+        }
+    }
+
+    if(flag == 1)
+        printf("Identity Matrix");
+    else
+        printf("Not an Identity Matrix");
+
+    return 0;
+}
